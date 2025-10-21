30edfe98611107b2188def24718d92fd8e69be95
Mannie <mannietech817@gmail.com>
2025-10-21 15:21:59 +0200

    mannie-chatbot

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 window.CONFIG = {
-    OPENROUTER_API_KEY: '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'
+    OPENROUTER_API_KEY: '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'
 };
